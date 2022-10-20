af4d783fde0b6be2d0b9d87394c3347ee823da16
Muhammed Furkan <mfyilmaz.hukuk@gmail.com>
2022-10-20 22:48:53 +0300

    v2

---
+++ src/CreationalDP/Abstract_FactoryDP/ColorFactory.java
@@ -0,0 +1,13 @@
+package CreationalDP.Abstract_FactoryDP;
+
+public class ColorFactory extends AbstractFactory{
+    @Override
+    Color getColor(String color) {
+        return null;
+    }
+
+    @Override
+    Shape getShape(String shape) {
+        return null;
+    }
+}
